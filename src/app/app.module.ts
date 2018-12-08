30e79c56a9da1ec7918e21baee0a393076bdebee
安石 <>
2018-12-08 18:55:10 +0800

    add markdown support
    ` https://github.com/jfcere/ngx-markdown `

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {MarkdownModule} from "ngx-markdown";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    MarkdownModule.forRoot(),
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -8,6 +8,18 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
+
+  content = '我还是习惯\n' +
+    '在睡前含一块糖\n' +
+    '进去甜甜的梦\n' +
+    '带着你的叮嘱\n' +
+    '你的斥责\n' +
+    '你的味道\n' +
+    '早安\n' +
+    '\n' +
+    '孤独小美\n' +
+    '格致别趣\n' +
+    '六点起床第630天 迟到38天'
   items: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
